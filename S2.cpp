c6520128897d296df64b7fb351b25a03bf84e725
wintersun661 <47968103+wintersun661@users.noreply.github.com>
2019-09-08 22:46:45 -0400

    Add files via upload
    1.enlarge the pre-computed distance-map(add center value into count)
    2.implement time-compute

--- S2.cpp
+++ S2.cpp
@@ -3,9 +3,12 @@
 #include <fstream>
 #include <cmath>
 #include <unordered_map>
-#include <sys/time.h>
-#include <unistd.h>
+#include <time.h>
 #include <cstdio>
+#include <cmath>
+#include <string>
+
+
 using namespace std;
 
 #define INF unsigned(-1)
@@ -15,36 +18,48 @@ const unsigned long long MIN = (unsigned long long) 60 * SEC;
 const unsigned long long HOUR = (unsigned long long) 60 * MIN;
 const unsigned long long DAY = (unsigned long long) 24 * HOUR;
 
+
 string qwerty = "qwertyuiopasdfghjklzxcvbnm"; // QWERTY layout
 string qwertyHandArray = "LLLLLRRRRRLLLLLRRRRLLLLRRR"; // first, second and third row in order, corresponding to QWERTY layout
+
+//same-left a,b,c;same right a,b,c; alternatetoleft a1,b1,a2,b2,c,d;alternatetoright a1,b1,a2,b2,c,d
+const double parameters[] = {36.7,-89.0,319.5,
+							13.8,-7.6,237.3,
+							2.003,-9.502,332200.,-0.024,-0.007,265.286,
+							-17.949,86.564,193000.,0.062,-0.035,142.601};
 
 vector<string> word_list;
 int LR, LC, RR, RC;
 int sizeL, sizeR;
+//width for calculating on both hands
+double widthL,widthR;
 
 unordered_map<string, double> top_list;
+
 // original coordinates
 double coor_org_row[26];
 double coor_org_column[26];
 // stretched coordinates
+//the center pos on stretched keyboard
+double center[2];
 double coor_L_row[26];
 double coor_L_column[26];
 double coor_R_row[26];
 double coor_R_column[26];
 // calculated distance map
-double L_distmap[26][26];
-double R_distmap[26][26];
+double L_distmap[27][26];
+double R_distmap[27][26];
 
 
 // unordered_map<unsigned, string> handList;
 // unordered_map<unsigned, vector<string *>> handAndWord;
-
+/*
 inline long long time_stamp() {
     struct timeval query_time;
     gettimeofday(&query_time, nullptr);
     return query_time.tv_sec * 1000000 + query_time.tv_usec;
 }
-
+*/
 int mapLetter(char c) {
 	switch(c) {
 	case 'q':
@@ -102,7 +117,7 @@ int mapLetter(char c) {
 	}
 }
 
-// 读取文件中的单词，并存储在word_list中
+
 void readWords(string filename) {
 	ifstream fin(filename);
 	string line;
@@ -110,7 +125,7 @@ void readWords(string filename) {
 	string numS;
 	int count = 0;
 	while (getline(fin, line)) {
-		if (count >= 3500) {
+		if (count >10) {
 			break;
 		}
 		int i;
@@ -129,7 +144,7 @@ void readWords(string filename) {
 }
 
 // calculate LR, LC, RR, RC
-void maxCR() {
+void maxCR(double* fingerTip){
 	LR = 0;
 	RR = 0;
 
@@ -192,8 +207,24 @@ void maxCR() {
 	RC = tmp > R1 ? tmp : R1; // find max of R1, R2, R3
 	RC++;
 
-	sizeL = (14.2/LR) * (22.8/LC);
-	sizeR = (14.2/RR) * (22.8/RC);
+	//wL: width of the key block on left(on row dimension)
+	//hL: height of the key block on left(on column dimension)
+	double rL = fingerTip[0]/LR;
+	double cL = fingerTip[1]/LC;
+	widthL = rL<cL?rL:cL;
+	//wR: width of the key block on right(on row dimension)
+	//hR: height of the key block on right(on column dimension)
+	double rR = fingerTip[0]/RR;
+	double cR = fingerTip[1]/RC;
+	widthR = rR<cR?rR:cR;
+
+	center[0] = fingerTip[0]*0.5;
+	center[1] = fingerTip[1]*0.5;
+
+	sizeL = rL * cL;
+	sizeR = rR * cR;
+
+	
 }
 
 // 提前计算正常QWERTY键位坐标
@@ -217,6 +248,34 @@ void calOriginalCoor() {
 		coor_org_column[char_idx] = i - 19 + 1.5;
 	}
 }
+
+
+double calTime(const char hand, double distance,double targetWidth,double idleTime){
+	double result;
+	double indexDifficulty = log2(distance/targetWidth+1);
+	cout<<"id: "<<indexDifficulty<<endl;
+	switch(hand){
+	case('L'):
+			if(idleTime == 0)
+				return parameters[0]*indexDifficulty*indexDifficulty+parameters[1]*indexDifficulty+parameters[2];
+			result = parameters[6]*indexDifficulty*indexDifficulty+parameters[7]*indexDifficulty+
+					parameters[8]*idleTime*idleTime+parameters[9]*idleTime+parameters[10]*indexDifficulty*idleTime+parameters[11];
+			return result;
+
+
+	case('R'):
+			if(idleTime == 0)
+				return parameters[3]*indexDifficulty*indexDifficulty+parameters[4]*indexDifficulty+parameters[5];
+			result = parameters[12]*indexDifficulty*indexDifficulty+parameters[13]*indexDifficulty+
+					parameters[14]*idleTime*idleTime+parameters[15]*idleTime+parameters[16]*indexDifficulty*idleTime+parameters[17];
+			return result;
+
+	default:
+		cout<<"illegal hand label!";
+		return -1;}
+
+}
+
 
 // 提前计算拉伸后的键位坐标
 void calRealCoor() {
@@ -254,42 +313,56 @@ void calDist() {
 			}
 		}
 	}
+	for(int j = 0;j<26;j++){
+		L_distmap[26][j] = euc_dist(center[0], center[1], coor_L_row[j], coor_L_column[j]);
+		R_distmap[26][j] = euc_dist(center[0], center[1], coor_R_row[j], coor_R_column[j]);
+	}
 }
 
-// 算layouttime
 double getLayoutTime() {
 	double total = 0;
-	// 算每个词所用时间
+
 	for (string& word : word_list) {
 		cout << "word:" << word <<endl;
-		for (int i = 0; i < word.length()-1; i++ ) {
-			// word[i] 和 word[i+1]是current letter和next letter
-			// 先根据layout和两个字母 判断是否要换边
+		//initialize
+		int letterL = 26+'a';
+		int letterR = 26+'a';
+
+		double handIdleTimeL = 0;
+		double handIdleTimeR = 0;
+
+		double distance;
+		double time;
+
+		for (int i = 0; i < word.length(); i++ ) {
+			// word[i]current letter
 			char current_hand = qwertyHandArray[mapLetter(word[i])];
-			char next_hand = qwertyHandArray[mapLetter(word[i+1])];
-			// 不换边的话
-			if (current_hand == next_hand){
-				if (current_hand == 'L'){
-					// 算出两个键在左手的距离 mm为单位
-					// 这里我只算了距离 ->这个可以当作是D 然后根据 MT公式算出所得最后正确时间
-					total += L_distmap[word[i]-'a'][word[i+1]-'a'];
-					cout << "c&n bothL == " << L_distmap[word[i]-'a'][word[i+1]-'a'] << endl;
-					cout << "total: " << total << endl;
-				}else{
-					// 算出两个键在右手的距离 mm为单位
-					// 这里我只算了距离 ->这个可以当作是D 然后根据 MT公式算出所得最后正确时间
-					total += R_distmap[word[i]-'a'][word[i+1]-'a'];
-					cout << "c&n bothR == " << L_distmap[word[i]-'a'][word[i+1]-'a'] << endl;
-					cout << "total: " << total << endl;
-				}
-			}else{
-				// 换边情况全算了1
-				cout << "switch " << endl;
-				total += 1;
+			
+			if (current_hand == 'L'){
+				distance =  L_distmap[letterL-'a'][word[i]-'a'];
+				letterL = word[i];
+				cout << "distance " << distance << ", width "<<widthL<<",idletime "<<handIdleTimeL<<endl;
+				time = calTime('L',distance,widthL,handIdleTimeL)/1000.;
+				cout<<word[i]<<" "<<current_hand<<" time :"<<time<<endl;
+				total+= time;
+				handIdleTimeL+=time;
+				handIdleTimeL = 0;
 				cout << "total: " << total << endl;
+			}else{
+				distance =  R_distmap[letterR-'a'][word[i]-'a'];
+				letterR = word[i];
+				cout << "distance " << distance << ", width "<<widthR<<",idletime "<<handIdleTimeR<<endl;
+				time = calTime('R',distance,widthR,handIdleTimeR)/1000.;
+				cout<<word[i]<<" "<<current_hand<<" time :"<<time<<endl;
+				
+				total+=time;
+				handIdleTimeL+=time;
+				handIdleTimeR = 0;
+				cout << "total: " << total << endl;
+				}
 			}
 		}
-	}
+	
 	return total;
 }
 
@@ -297,8 +370,9 @@ int main(int argc, char const *argv[])
 {
 	// process keyboard
 	calOriginalCoor();
-	long long start = time_stamp();
-	maxCR();
+	//long long start = time_stamp();
+	double fingerSize[2] = {14.2,22.8};
+	maxCR(fingerSize);
 	calRealCoor();
 	calDist();
 	cout << "keyboard processed" << endl;
@@ -310,8 +384,8 @@ int main(int argc, char const *argv[])
 	// get the ans
 	double ans = getLayoutTime();
 	cout << "ans: " << ans << endl;
-	long long end = time_stamp();
-	printf("simulation time: %lf\n", (end - start) / (double)1000000);
+	//long long end = time_stamp();
+	//printf("simulation time: %lf\n", (end - start) / (double)1000000);
 	cout << "simulation done!" << endl;
 
 
